b2c9db8dced42f6792977bd045d6f836bf5a517a
Yunus Tan <yunus.tan@outlook.com.tr>
2025-12-09 22:41:48 +0300

    feat: add main plugin file with header metadata

---
+++ flare-press.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: FlarePress
+Description: WordPress plugin for uploading media directly to Cloudflare Images alongside the default uploader.
+Version:     0.1.0
+Author:      Yunus Tan
+Author URI:  https://github.com/tanyunus/
+License:     GPL-2.0+
+Text Domain: flare-press
+*/
+

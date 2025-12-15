c642982a36746c76d6d647f8a4d6122ea6805db6
Yunus Tan <yunus.tan@outlook.com.tr>
2025-12-15 22:31:30 +0300

    chore: Constant controller introduced

---
+++ includes/Controllers/FPConstants.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace FP\Controllers;
+
+class FPConstants
+{
+    //UPLOAD
+    public const UPLOAD_TO_CF_INDICATOR = 'fp_upload_to_cf';
+    public const UPLOADED_IMAGE_CF_ID_NAME = 'fp_cf_image_id';
+
+    // CF API
+    public const CF_API_URL = 'https://api.cloudflare.com/client/v4/accounts/';
+    public const CF_CDN_URL = 'https://imagedelivery.net/';
+    public const CF_API_VERSION = 'v1';
+    public const CF_API_MODULE_VARIANTS = '/variants';
+
+    // Views
+    public const DASHBOARD_VIEW = 'admin/dashboard';
+    public const DASHBOARD_MENU_SLUG = 'flare-press-settings';
+    public const DASHBOARD_MENU_TITLE = 'FlarePress';
+    public const DASHBOARD_PAGE_TITLE = 'FlarePress Settings';
+    public const DASHBOARD_SETTINGS_GROUP_NAME = 'fp_settings_group';
+    public const DASHBOARD_SECTION_ID = 'fp_settings_section_id';
+    public const DASHBOARD_CF_ACCOUNT_ID_FIELD_NAME = 'fp_cf_account_id';
+    public const DASHBOARD_CF_ACCOUNT_HASH_FIELD_NAME = 'fp_cf_account_hash';
+    public const DASHBOARD_CF_API_TOKEN_FIELD_NAME = 'fp_cf_api_token';
+}

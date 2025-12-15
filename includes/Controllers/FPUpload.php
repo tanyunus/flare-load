af94a8bb05a3f5eb0f6f36953fcede0b373a1327
Yunus Tan <yunus.tan@outlook.com.tr>
2025-12-15 23:28:42 +0300

    feat: main upload controller introduced

---
+++ includes/Controllers/FPUpload.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FP\Controllers;
+
+use Exception;
+
+class FPUpload
+{
+    public function __construct()
+    {
+        // Actions to be taken right after attachment added
+        add_action('add_attachment', function ($attachmentId) {
+            if (!$this->isAttachmentToBeUploadedToCf()) {
+                return;
+            }
+
+            try {
+                $imageFile = get_attached_file($attachmentId);
+                $fileName = basename($imageFile);
+                $cfUploadResult = FPCFImagesApi::uploadSingleImage($imageFile, $fileName);
+                $publicVariantUrl = FPCFImagesApi::getVariantUrl('public', $cfUploadResult['result']['id']);
+
+                FPUploadModifier::updateAttachmentGuid($attachmentId, $publicVariantUrl);
+                FPUploadModifier::updateAttachmentFileValue($attachmentId, $publicVariantUrl);
+
+                // Actions to be taken right after attachment meta added
+                add_filter('wp_generate_attachment_metadata', function ($metadata, $attachmentId, $context) use ($cfUploadResult, $publicVariantUrl) {
+                    $cfVariants = FPCFImagesApi::getVariants();
+
+                    $newMetadata = FPUploadModifier::updateAttachmentMeta(
+                        $attachmentId,
+                        $metadata,
+                        $publicVariantUrl,
+                        $cfUploadResult['result']['id'],
+                        $cfVariants
+                    );
+
+                    error_log(print_r($newMetadata, true));
+
+                    return $newMetadata;
+                }, 10, 3);
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
+        });
+    }
+
+    private function isAttachmentToBeUploadedToCf(): bool
+    {
+        return $_POST[FPConstants::UPLOAD_TO_CF_INDICATOR] ?? false;
+    }
+}

78977cd7b2b7b657ba1c74e106fcf7b226812332
Yunus Tan <yunus.tan@outlook.com.tr>
2025-12-16 02:06:27 +0300

    chore: list controller updated

--- includes/Controllers/FPList.php
+++ includes/Controllers/FPList.php
@@ -2,6 +2,8 @@
 
 namespace FP\Controllers;
 
+use FP\Utils\AdminPage;
+
 class FPList
 {
     public function __construct() {
@@ -23,17 +25,72 @@ class FPList
                     if(isset($response['sizes']['thumbnail'])) {
                         $response['sizes']['thumbnail']['url'] = $imgUrl;
                     }
-
                 }
 
                 return $response;
             }, 10, 3);
         }, 1);
+
+
+        add_action('wp_print_scripts', function() {
+            if(AdminPage::is('upload.php')) {
+                $this->addCfImageIdsToWindow();
+            }
+        });
+
     }
 
     private function isCfImage(int $attachmentId): bool {
         $attachmentMeta = wp_get_attachment_metadata($attachmentId);
 
-        return isset($attachmentMeta['fp_cf_image_id']);
+        return isset($attachmentMeta[FPConstants::UPLOADED_IMAGE_CF_ID_NAME]);
+    }
+
+    private function getCfImages(): array {
+        $attachments = get_posts(array(
+            'post_type'      => 'attachment',
+            'posts_per_page' => -1,
+            'post_status'    => 'inherit',
+            'meta_query'     => array(
+                array(
+                    'key'     => '_wp_attachment_metadata',
+                    'value'   => FPConstants::UPLOADED_IMAGE_CF_ID_NAME,
+                    'compare' => 'LIKE'
+                )
+            )
+        ));
+
+        $filteredAttachments = [];
+
+        foreach($attachments as $attachment) {
+            $metaData = wp_get_attachment_metadata($attachment->ID);
+
+            if(isset($metaData[FPConstants::UPLOADED_IMAGE_CF_ID_NAME])) {
+                $filteredAttachments[] = $attachment;
+            }
+        }
+        error_log(print_r($filteredAttachments, true));
+        return $filteredAttachments;
+    }
+
+    private function getCfImageIds(): array {
+        $imageIds = [];
+
+        foreach ($this->getCfImages() as $image) {
+            $imageIds[] = $image->ID;
+        }
+
+        return $imageIds;
+    }
+
+    private function addCfImageIdsToWindow(): void {
+        $jsonEncodedIds = implode(",", $this->getCfImageIds());
+
+        ?>
+            <script>
+                window.fp = window.fp || {};
+                window.fp.cfImageIds = [<?php echo $jsonEncodedIds; ?>];
+            </script>
+        <?php
     }
 }

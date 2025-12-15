64d5a3fd976918196a65a8b2011f4619696ebb73
Yunus Tan <yunus.tan@outlook.com.tr>
2025-12-15 22:32:03 +0300

    feat: dashboard controller introduced

--- includes/Controllers/FPDashboard.php
+++ includes/Controllers/FPDashboard.php
@@ -5,15 +5,6 @@ namespace FP\Controllers;
 use FP\Utils\Template;
 
 class FPDashboard {
-    const DASHBOARD_VIEW = 'admin/dashboard';
-    const MENU_SLUG = 'flare-press-settings';
-    const MENU_TITLE = 'FlarePress';
-    const PAGE_TITLE = 'FlarePress Settings';
-    const SETTINGS_GROUP_NAME = 'fp_settings_group';
-    const SECTION_ID = 'fp_settings_section_id';
-    const CF_ACCOUNT_ID_FIELD_NAME = 'fp_cf_account_id';
-    const CF_API_TOKEN_FIELD_NAME = 'fp_cf_api_token';
-
     public function __construct() {
         add_action( 'admin_menu', [$this, 'initFPSettings'] );
     }
@@ -22,22 +13,23 @@ class FPDashboard {
         $this->addSettingsPage();
         $this->addSection();
         $this->registerAccountIDField();
+        $this->registerAccountHashField();
         $this->registerAPITokenField();
     }
 
     private function addSettingsPage(): void {
         add_options_page(
-            self::PAGE_TITLE,
-            self::MENU_TITLE,
+            FPConstants::DASHBOARD_PAGE_TITLE,
+            FPConstants::DASHBOARD_MENU_TITLE,
             'manage_options',
-            self::MENU_SLUG,
+            FPConstants::DASHBOARD_MENU_SLUG,
             [$this, 'fpAdminDashboardView'],
             5
         );
     }
 
     public function fpAdminDashboardView(): void {
-        Template::render(self::DASHBOARD_VIEW);
+        Template::render(FPConstants::DASHBOARD_VIEW);
     }
 
     private function addSection(): void {
@@ -45,36 +37,51 @@ class FPDashboard {
             'fp_settings_section_id',
             '',
             '',
-            self::MENU_SLUG
+            FPConstants::DASHBOARD_MENU_SLUG
         );
     }
 
     private function registerAccountIDField(): void {
-        register_setting(self::SETTINGS_GROUP_NAME, self::CF_ACCOUNT_ID_FIELD_NAME, [$this, 'sanitizeText']);
+        register_setting(FPConstants::DASHBOARD_SETTINGS_GROUP_NAME, FPConstants::DASHBOARD_CF_ACCOUNT_ID_FIELD_NAME, [$this, 'sanitizeText']);
 
         add_settings_field(
-            self::CF_ACCOUNT_ID_FIELD_NAME,
+            FPConstants::DASHBOARD_CF_ACCOUNT_ID_FIELD_NAME,
             'Cloudflare Account ID',
             function() {
-                $this->renderInputField(self::CF_ACCOUNT_ID_FIELD_NAME);
+                $this->renderInputField(FPConstants::DASHBOARD_CF_ACCOUNT_ID_FIELD_NAME);
             },
-            self::MENU_SLUG,
-            self::SECTION_ID,
+            FPConstants::DASHBOARD_MENU_SLUG,
+            FPConstants::DASHBOARD_SECTION_ID,
+        );
+    }
+
+    private function registerAccountHashField(): void {
+        register_setting(FPConstants::DASHBOARD_SETTINGS_GROUP_NAME, FPConstants::DASHBOARD_CF_ACCOUNT_HASH_FIELD_NAME, [$this, 'sanitizeText']);
+
+        add_settings_field(
+                FPConstants::DASHBOARD_CF_ACCOUNT_HASH_FIELD_NAME,
+                'Cloudflare Account Hash',
+                function() {
+                    $description = 'You can find it under <i>https://dash.cloudflare.com/<b>your-account-id-here</b>/images/hosted</i>';
+                    $this->renderInputField(FPConstants::DASHBOARD_CF_ACCOUNT_HASH_FIELD_NAME, $description);
+                },
+                FPConstants::DASHBOARD_MENU_SLUG,
+                FPConstants::DASHBOARD_SECTION_ID,
         );
     }
 
     private function registerApiTokenField(): void {
-        register_setting(self::SETTINGS_GROUP_NAME, self::CF_API_TOKEN_FIELD_NAME, [$this, 'sanitizeText']);
+        register_setting(FPConstants::DASHBOARD_SETTINGS_GROUP_NAME, FPConstants::DASHBOARD_CF_API_TOKEN_FIELD_NAME, [$this, 'sanitizeText']);
 
         add_settings_field(
-            self::CF_API_TOKEN_FIELD_NAME,
+            FPConstants::DASHBOARD_CF_API_TOKEN_FIELD_NAME,
             'Cloudflare Account API Token',
             function() {
                 $description = 'You can find it under <i>https://dash.cloudflare.com/<b>your-account-id-here</b>/api-tokens</i>';
-                $this->renderInputField(self::CF_API_TOKEN_FIELD_NAME, $description);
+                $this->renderInputField(FPConstants::DASHBOARD_CF_API_TOKEN_FIELD_NAME, $description);
             },
-            self::MENU_SLUG,
-            self::SECTION_ID,
+            FPConstants::DASHBOARD_MENU_SLUG,
+            FPConstants::DASHBOARD_SECTION_ID,
         );
     }
 
--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -1,3 +1,10 @@
 <div class="wrap">
-    <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+    <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+    <form id="fp_options_form" method="post" action="options.php">
+        <?php
+        settings_fields( 'fp_settings_group' );
+        do_settings_sections( 'flare-press-settings' );
+        submit_button();
+        ?>
+    </form>
 </div>

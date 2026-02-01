c9c51b834127283f81099a5814828f4bbed6b5f0
Yunus Tan <yunus.tan@outlook.com.tr>
2026-02-01 22:06:59 +0300

    chore: dev github profile link added to header

--- app/Views/header.php
+++ app/Views/header.php
@@ -2,7 +2,8 @@
     <img height="120" src="<?php echo WP_PLUGIN_DIR . '/flare-press/includes/dist/images/fp_logo.png' ?>">
     <div>
         <p>DIRECT<br>CLOUDFLARE IMAGES<br>INTEGRATION</p>
-        <a href="https://flare-press.com">flare-press.com</a>
+        <a target="_blank" title="FlarePress plugin website" href="https://flare-press.com">Website</a>&nbsp;&nbsp;
+        <a target="_blank" title="FlarePress developer Github profile" href="https://github.com/tanyunus">Github</a>
     </div>
 </div>
 <hr>

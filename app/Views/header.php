cacc854633de317e4ffc45be39b7d223f99d0945
Yunus Tan <yunus.tan@outlook.com.tr>
2026-05-01 20:53:28 +0300

    fix: allow br tags in header tagline output

--- app/Views/header.php
+++ app/Views/header.php
@@ -2,7 +2,7 @@
 <div class="fp-header">
     <img height="120" src="<?php echo esc_url(FLARE_PRESS_URL . 'dist/images/fp_logo.png') ?>">
     <div>
-        <p><?php echo esc_html(Utils::localize(Constants::UI_HEADER_TAGLINE)); ?></p>
+        <p><?php echo wp_kses(Utils::localize(Constants::UI_HEADER_TAGLINE), ['br' => []]); ?></p>
         <a target="_blank" title="<?php echo esc_attr(Utils::localize(Constants::UI_HEADER_WEBSITE_LINK_TITLE)); ?>" href="https://flare-press.com">Website</a>&nbsp;&nbsp;
         <a target="_blank" title="<?php echo esc_attr(Utils::localize(Constants::UI_HEADER_GITHUB_LINK_TITLE)); ?>" href="https://github.com/tanyunus">Github</a>
     </div>

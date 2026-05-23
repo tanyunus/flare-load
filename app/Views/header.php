5767ada8ca80724badbb488008c70df56d73fe31
Yunus Tan <yunus.tan@outlook.com.tr>
2026-05-23 23:44:11 +0300

    style: remove website link from options page header

--- app/Views/header.php
+++ app/Views/header.php
@@ -1,6 +1,6 @@
 <?php defined('ABSPATH') || exit; ?>
 <div class="flareload-header">
-    <img height="120" src="<?php echo esc_url(FLARELOAD_URL . 'dist/images/FLARELOAD_logo.png') ?>">
+    <img height="120" src="<?php echo esc_url(FLARELOAD_URL . 'dist/images/flareload_logo.png') ?>">
     <div>
         <p><?php echo wp_kses(__('DIRECT<br>CLOUDFLARE IMAGES<br>INTEGRATION', 'flare-load'), ['br' => []]); ?></p>
         <a target="_blank" title="<?php echo esc_attr(__('FlareLoad developer Github profile', 'flare-load')); ?>" href="https://github.com/tanyunus">Github</a>
